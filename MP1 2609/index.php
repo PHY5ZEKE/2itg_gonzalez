2664b0347c81f5ea1edf7f9123ed2cb819c19795
Ezequiel Gonzalez <124253644+PHY5ZEKE@users.noreply.github.com>
2023-02-17 20:12:28 +0800

    Added Number Format and Updated CSS
    -number format
    -rounded borders with shadows
    -new font
    -centered logo
    -animations

--- MP1 2609/index.php
+++ MP1 2609/index.php
@@ -3,33 +3,36 @@
     <head>
         <title>Kaltas:Income Tax Calculator</title>
         <!--CSS-->
-        <link href="./css/style.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
         <!--Bootstrap-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <!--Font-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Playfair+Display&display=swap" rel="stylesheet">
     </head>
 
     <body>
         <!--CSS-->
-        <link href="./css/style.css" rel="stylesheet">
-
-        <nav class="navbar-custom">
+        <link href="style.css" rel="stylesheet">
+    <nav class="navbar-custom">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">
-                <img src="kaltasLogo.png" alt="Logo" height="50" class="d-inline-block align-text-top">
-                
+                <img src="logo.png" alt="Logo" height ="60" class="d-inline-block align-text-top">
               </a>
             </div>
           </nav>
           <br>
-       <div class ="container">
+       <div class ="box">
+        
             <div class="panel">
               <div class="header">
-               <center> <h2 class="headline">Income Tax Calculator</h2></center>
+               <center> <h2>Income Tax Calculator</h2></center>
               </div>
               <div class="calculator">
                 <form action ="index.php" method="get">
                     <div class="row mb-3">
-                      <label for="inputSalary" class="col-sm-2 col-form-label">Salary</label>
+                      <label for="inputSalary" class="col-sm-2 col-form-label d-flex justify-content-center">Salary:</label>
                       <div class="col-sm-10">
                         <input type="number" class="form-control" id="inputSalary" name ="salary" required>
                       </div>
@@ -85,7 +88,7 @@
                                 $annualTax = 130000+$excess*0.30;
                                 $monthlyTax = $annualTax/12;
                             }
-                            else if($salary >2000000.0 && $salary <= 8000000000.0)
+                            else if($salary >2000000.0 && $salary <= 8000000.0)
                             {
                                 $excess =  $salary-2000000.0;
                                 $annualTax = 490000+$excess*0.32;
@@ -97,9 +100,9 @@
                                 $annualTax = 2410000+$excess*0.35;
                                 $monthlyTax = $annualTax/12;
                             }
-                            echo "Annual Salary: PHP $salary<br>"; 
-                            echo "Est Annual Tax: PHP $annualTax<br>";
-                            echo "Est Monthly Tax: PHP $monthlyTax<br>" ;
+                            echo "Annual Salary: PHP ".number_format($salary,2)."<br>"; 
+                            echo "Est Annual Tax: PHP ".number_format($annualTax,2)."<br>";
+                            echo "Est Monthly Tax: PHP ".number_format($monthlyTax,2)."<br>" ;
                         }
                         if($salaryType == "Bi-monthly" )
                         {
@@ -127,21 +130,21 @@
                                 $annualTax = 130000+$excess*0.30;
                                 $monthlyTax = $annualTax/12;
                             }
-                            else if($salary >2000000.0 && $salary <= 8000000000.0)
+                            else if($salary >2000000.0 && $salary <= 8000000.0)
                             {
                                 $excess =  $salary-2000000.0;
                                 $annualTax = 490000+$excess*0.32;
                                 $monthlyTax = $annualTax/12;
                             }
-                            else if($salary >8000000.0)
+                            else if($salary > 8000000.0)
                             {
                                 $excess =  $salary-8000000.0;
-                                $annualTax = 2410000+$excess*0.35;
+                                $annualTax = 2410000+($excess*0.35);
                                 $monthlyTax = $annualTax/12;
                             }
-                            echo "Annual Salary: PHP $salary<br>"; 
-                            echo "Est Annual Tax: PHP $annualTax<br>";
-                            echo "Est Monthly Tax: PHP $monthlyTax<br>" ;
+                            echo "Annual Salary: PHP ".number_format($salary,2)."<br>"; 
+                            echo "Est Annual Tax: PHP ".number_format($annualTax,2)."<br>";
+                            echo "Est Monthly Tax: PHP ".number_format($monthlyTax,2)."<br>" ;
                         }
                             
     }
